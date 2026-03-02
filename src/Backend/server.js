d72986744e4bb75b6dce3501a6013d5bef55934a
rutu48836-create <rutu48836@gmail.com>
2026-03-02 21:02:57 +0530

    Update server.js

--- src/Backend/server.js
+++ src/Backend/server.js
@@ -7,7 +7,6 @@ import { Chat_handler } from './controllers/chatController.js'
 import chatRoutes from './routes/chat.js'
 import { scrapeWebsite } from './services/website_data.js'
 import { extractPDFText } from './services/pdf_data.js'
-import { Create_checkout_session,Stripe_webhook} from './routes/stripe.js'
 
 dotenv.config()
 
