1a7a4d78f5a1131eebdf1ca7fcc6c614e8ea0145
rutu48836-create <rutu48836@gmail.com>
2026-03-02 20:59:45 +0530

    Update chatController.js

--- src/Backend/controllers/chatController.js
+++ src/Backend/controllers/chatController.js
@@ -71,14 +71,19 @@ Your goal is to be helpful, clear, and human-like.
  console.log(`chat bot name ${chatbot.name}`)
  
 
-const { error: usageError } = await supabase.rpc(
+const { data,error } = await supabase.rpc(
   "check_and_increment_message",
   { uid: chatbot.user_id }
 )
 
-if (usageError) {
+if (error) {
   return res.status(403).json({
-    error: "Monthly limit reached. Upgrade to Pro."
+    error: "RPC FAILED."
+  })
+
+	if (!data) {
+  return res.status(403).json({
+    error: "Monthly limit reached"
   })
 }
 
