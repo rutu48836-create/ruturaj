bc34f57de370479c5c5a105721a1f377e82d2ea3
rutu48836-create <rutu48836@gmail.com>
2026-04-12 18:22:52 +0530

    Update ChatPage.jsx

--- src/Frontend/Pages/ChatPage.jsx
+++ src/Frontend/Pages/ChatPage.jsx
@@ -69,6 +69,7 @@ historyRef.current.push({ role: 'user', content: message })
   const hex = parseInt(bgColor.replace('#', ''), 16)
   if(hex < 0x918989) return '#ffffff'
   else if(hex < 0xcccccc) return '#000000'
+        else if(hex < #dbd7d7)  return '#000'  
   else return '#ffffff'
 }
 
